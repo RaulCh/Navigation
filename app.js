ba8d357f6ef648eca41e8d456df9651fb3bfdbd1
RaulCh <raul.chavez.ramos@gmail.com>
2013-01-09 11:32:57 -0600

    Update app.js

--- app.js
+++ app.js
@@ -14,16 +14,6 @@ $('#tab-bar a').on('click', function(e){
 function page(toPage) {
     var toPage = $(toPage),
     fromPage = $("#pages .current");
-    toPage.addClass("current fade in").one("webkitAnimationEnd", function(){
-        fromPage.removeClass("current fade out");
-        toPage.removeClass("fade in")
-    });
-    fromPage.addClass("fade out");
-}
-
-function page(toPage) {
-    var toPage = $(toPage),
-    fromPage = $("#pages .current");
     if(toPage.hasClass("current") || toPage === fromPage) {
         return;
     };
